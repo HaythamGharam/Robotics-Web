4db044851bc0fcce1cbcd812863e1cff82da15aa
Bushm4ster <70144400+Bushm4ster@users.noreply.github.com>
2021-11-27 23:30:51 +0100

    navbar completed and unfinished members popup

---
+++ script.js
@@ -0,0 +1,38 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+};
+cancelBtn.onclick = () => {
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
+
+//popup region start
+var modal = document.getElementById("popup");
+var btn = document.getElementById("btn");
+var exit = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+exit.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+//popup region end
