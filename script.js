c15cdddb17c3bb31f79503ab171eda3d73aeaf2d
Bushm4ster <70144400+Bushm4ster@users.noreply.github.com>
2021-12-06 16:11:19 +0100

    Revert "Go check Script.js Line 22 Check Comments you will understand"
    This reverts commit 9fa08f735ffb4696b56197f631924671e334ed3b.

--- script.js
+++ script.js
@@ -19,23 +19,13 @@ window.onscroll = () => {
 };
 
 //popup region start
-var modal = document.getElementsByClassName("popup"); // ell erreur mte3ek houni 5ater el Model dima Ye54 fi awel wa7da mawjoud fiha Popup
+var modal = document.getElementById("popup");
 var btn = document.getElementById("btn");
 var exit = document.getElementsByClassName("close")[0];
-
 // Zedt he4i  esta3mel el JQuery 
-$(document).ready(function(){
-  $(document).on("click","#btn1",function(){
-      modal.style.display = "block";
-  });
-  $(document).on("click","#btn2",function(){
-      modal.style.display = "block";
-  });
-
+$(document).on("click","#btn",function(){
+  modal.style.display = "block";
 });
-
-
-
 // btn.onclick = function () {
 //   modal.style.display = "block";
 // };
